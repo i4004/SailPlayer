530d014a318c192f0c0d3a0eba902591ec33e876
Alexanderius <alexander.krylkov@gmail.com>
2015-07-26 21:01:33 +0600

    EqualizerModel model implemented

--- src/SailPlayer/src/Equalizer/EqualizerModel.cpp
+++ src/SailPlayer/src/Equalizer/EqualizerModel.cpp
@@ -4,6 +4,10 @@ namespace Equalizer
 {
 	EqualizerModel::EqualizerModel(QObject* parent) : QAbstractListModel(parent)
 	{
+		_rolesNames = QAbstractListModel::roleNames();
+		_rolesNames.insert(FrequencyRole, QByteArray("bandFrequency"));
+		_rolesNames.insert(WidthRole, QByteArray("bandWidth"));
+		_rolesNames.insert(GainRole, QByteArray("bandGain"));
 	}
 
 	EqualizerModel::~EqualizerModel()
@@ -11,14 +15,50 @@ namespace Equalizer
 		DeleteBands();
 	}
 
+	int EqualizerModel::rowCount(const QModelIndex &parent) const
+	{
+		Q_UNUSED(parent);
+
+		return _bands.count();
+	}
+
+	QVariant EqualizerModel::data(const QModelIndex &index, int role) const
+	{
+		if (!index.isValid() || index.row() >= _bands.count())
+			return QVariant();
+
+		EqualizerBand* item = _bands.at(index.row());
+
+		switch (role)
+		{
+			case Qt::DisplayRole:
+
+			case FrequencyRole:
+				return item->GetFrequency();
+
+			case WidthRole:
+				return item->GetWidth();
+
+			case GainRole:
+				return item->GetGain();
+
+			default:
+				return QVariant();
+		}
+	}
+
 	void EqualizerModel::SetBands(QList<EqualizerBand*> bands)
 	{
+		beginResetModel();
+
 		DeleteBands();
 
 		foreach (EqualizerBand* band, bands)
 		{
 			_bands.append(new EqualizerBand(*band));
 		}
+
+		endResetModel();
 	}
 
 	QList<EqualizerBand*> EqualizerModel::GetBands()
--- src/SailPlayer/src/Equalizer/EqualizerModel.hpp
+++ src/SailPlayer/src/Equalizer/EqualizerModel.hpp
@@ -7,6 +7,14 @@
 
 namespace Equalizer
 {
+	enum EqualizerRoles
+	{
+		FrequencyRole = Qt::UserRole + 1,
+		WidthRole = Qt::UserRole + 2,
+		GainRole = Qt::UserRole + 3
+	};
+
+
 	class EqualizerModel : public QAbstractListModel
 	{
 		Q_OBJECT
@@ -15,10 +23,15 @@ namespace Equalizer
 		explicit EqualizerModel(QObject* parent = 0);
 		~EqualizerModel();
 
+		int rowCount(const QModelIndex &parent = QModelIndex()) const;
+		QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+		QHash<int, QByteArray> roleNames() const { return _rolesNames; }
+
 		void SetBands(QList<EqualizerBand*> bands);
 		QList<EqualizerBand*> GetBands();
 
 	private:
+		QHash<int, QByteArray> _rolesNames;
 		QList<EqualizerBand*> _bands;
 
 		void DeleteBands();
